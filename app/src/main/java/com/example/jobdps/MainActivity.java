84077959009a3f6c18513f4ada9936bdab10d225
Rossybossy235 <parkerhomeross@gmail.com>
2023-04-09 15:47:57 -0500

    Add basic top app bar structure.

--- app/src/main/java/com/example/jobdps/MainActivity.java
+++ app/src/main/java/com/example/jobdps/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.jobdps;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    MaterialToolbar topAppBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        topAppBar = (MaterialToolbar) findViewById(R.id.topAppBar);
+
+        topAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+            // Handle navigation icon press
+
+        topAppBar.setOnMenuItemClickListener(new MaterialToolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+//                    case R.id.favorite:
+//                        // Handle favorite icon press
+//
+//                        break;
+
+                    case R.id.search:
+                        // Handle search icon press
+
+                        break;
+
+                    case R.id.more:
+                        // Handle more item (inside overflow menu) press
+
+                        break;
+                }
+                return true;
+            }
+
+
+        });
     }
 }
