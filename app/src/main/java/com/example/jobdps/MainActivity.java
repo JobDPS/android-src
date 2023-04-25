c254ff1cacc0a59b9ef6c6c7a73fad34ef3ad469
Rossybossy235 <parkerhomeross@gmail.com>
2023-04-25 12:02:22 -0500

    Add initial fragment page navigation functionality.

--- app/src/main/java/com/example/jobdps/MainActivity.java
+++ app/src/main/java/com/example/jobdps/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.view.Menu;
@@ -62,8 +65,10 @@ public class MainActivity extends AppCompatActivity {
         navBar.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                item.setChecked(true);
-                drawerLayout.close();
+//                item.setChecked(true);
+//                drawerLayout.close();
+//                return true;
+                selectDrawerFragment(item);
                 return true;
             }
         });
@@ -74,5 +79,47 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.top_app_bar, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // The action bar home/up action should open or close the drawer.
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void selectDrawerFragment(MenuItem menuItem) {
+        Fragment fragment = null;
+        Class fragmentClass;
+
+        switch(menuItem.getItemId()) {
+            case R.id.nav_item1:
+                fragmentClass = HomeFragment.class;
+                break;
+            case R.id.nav_item2:
+                fragmentClass = ExploreFragment.class;
+                break;
+            default:
+                fragmentClass = HomeFragment.class;
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.cLayout, fragment).commit();
+
+        menuItem.setChecked(true);
+        setTitle(menuItem.getTitle());
+
+        drawerLayout.close();
     }
 }
